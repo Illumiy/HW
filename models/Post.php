67b6cac5f0928f267fa479b0609041e52db1b270
ClassUser <ClassUser@spbrtk.local>
2019-09-11 13:23:29 +0300

    CRUD step

---
+++ models/Post.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+
+/**
+ * This is the model class for table "post".
+ *
+ * @property int $id
+ * @property string $title
+ * @property string $body
+ * @property string $created_at
+ * @property string $updated_at
+ * @property int $author_id
+ *
+ * @property User $author
+ */
+class Post extends \yii\db\ActiveRecord
+{
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'post';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['title', 'body', 'created_at', 'updated_at'], 'required'],
+            [['body'], 'string'],
+            [['created_at', 'updated_at'], 'safe'],
+            [['author_id'], 'integer'],
+            [['title'], 'string', 'max' => 255],
+            [['author_id'], 'exist', 'skipOnError' => true, 'targetClass' => User::className(), 'targetAttribute' => ['author_id' => 'id']],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'title' => 'Title',
+            'body' => 'Body',
+            'created_at' => 'Created At',
+            'updated_at' => 'Updated At',
+            'author_id' => 'Author ID',
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getAuthor()
+    {
+        return $this->hasOne(User::className(), ['id' => 'author_id']);
+    }
+}
